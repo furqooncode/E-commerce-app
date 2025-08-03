a26284354492cf2101ba80c87aebe9ba4a4509e4
furqooncode <opeyemifurqoon@gmail.com>
2025-08-03 20:24:24 +0100

    update

---
+++ product.js
@@ -0,0 +1,42 @@
+const purchase = document.getElementById('purchase');
+const description = document.getElementById('description');
+const image = document.getElementById('image');
+const price = document.getElementById('price');
+const name = document.getElementById('name');
+const namer = document.getElementById('namer');
+const pricer = document.getElementById('pricer');
+const images = document.getElementById('images')
+
+
+const items = JSON.parse(localStorage.getItem('items')) || [];
+
+if (items) {
+  description.textContent = items.description;
+   image.src = items.image;
+   name.textContent = items.title;
+   namer.textContent = items.title;
+ pricer.textContent = "$" + items.price;
+   price.textContent = "$" + items.price;
+   images.src = items.image;
+ }
+ 
+ 
+ const plus = document.getElementById('plus');
+ const minus = document.getElementById('minus')
+ 
+ 
+   let count = 1;
+
+ plus.addEventListener('click', function (event) {
+   event.preventDefault();
+   let num = document.getElementById('number');
+   let counted = ++count;
+   num.textContent = counted;
+   console.log(counted)
+ })
+ 
+  minus.addEventListener('click', function (event) {
+   let num = document.getElementById('number');
+   let counted = --count;
+   num.textContent = counted;
+ })
