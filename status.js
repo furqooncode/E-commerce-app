7248838c675b88cbd3f3bc923f086af21296f294
furqooncode <opeyemifurqoon@gmail.com>
2025-08-15 00:51:46 +0100

    update

--- status.js
+++ status.js
@@ -15,3 +15,12 @@ function cart(param) {
 function status(param) {
   window.location.href = 'status.html'
 }
+
+function back(param) {
+  window.location.href = 'index.html'
+}
+
+const history = document.getElementById('history');
+history.addEventListener('click', function (param) {
+   window.location.href = 'history.html'
+})
